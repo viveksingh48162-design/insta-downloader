66b4fcbb3c27c9453e9256d92816ce7b3d2b1f93
viveksingh48162-design <viveksingh48162@gmail.com>
2026-04-09 20:40:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -84,6 +84,7 @@ def download():
 
     return jsonify({"error": "All download servers busy ❌ Try again"})
 
-
 if __name__ == "__main__":
-    app.run()
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
